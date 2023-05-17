952524145c5fba0f8f111e71dec852eb63ab4a4c
Saymon Felipe <linnubr@gmail.com>
2023-05-16 23:02:12 -0300

    Termino galeriae pagina dos pastores

--- src/pages/galleryPage.vue
+++ src/pages/galleryPage.vue
@@ -7,7 +7,31 @@
                 <h3>Veja as fotos da nossa igreja</h3>
             </div>
         </div>
-        <optionsBackground>            
+        <optionsBackground style="width: 100%;">
+            <div class="gallery-container">
+                <div ref="swiper" class="swiper">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <img src="https://i0.wp.com/teologiabrasileira.com.br/wp-content/uploads/2018/08/o_culto_e_os_seus_princ%C3%ADpios_d_b_riker_2.jpg?fit=630%2C337&ssl=1" class="img-fluid">
+                            <p>Lorem ipsum dolor sit amet</p>
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="https://1.bp.blogspot.com/-utwAvmWIKAw/WI3xUrhS69I/AAAAAAAAWNk/0cPjCILSTWESBPmVYttXlwrQtN6LPKinACLcB/s640/culto-cristao-contemporaneo.jpg" class="img-fluid">
+                            <p>Lorem ipsum dolor sit amet</p>
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="https://www.portaldotrono.com/wp-content/uploads/2020/03/culto.jpg" class="img-fluid">
+                            <p>Lorem ipsum dolor sit amet</p>
+                        </div>
+                    </div>
+                    <div class="swiper-pagination"></div>
+
+                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-button-next"></div>
+
+                    <div class="swiper-scrollbar"></div>
+                </div>
+            </div>   
         </optionsBackground>
         <footerComponent />
     </div>
@@ -17,14 +41,87 @@ import headerComponent from "../components/headerComponent.vue";
 import optionsBackground from "../components/optionsBackground.vue";
 import footerComponent from "../components/footerComponent.vue";
 
+import Swiper, { Navigation, Pagination, EffectCards } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
+
 export default {
     name: "galleryPage",
     components: {
         headerComponent,
         optionsBackground,
         footerComponent
+    },
+    mounted: function () {
+        this.swiper = new Swiper(this.$refs.swiper, {
+            modules: [Navigation, Pagination, EffectCards],
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true
+            },
+            scrollbar: {
+                el: '.swiper-scrollbar',
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            slidesPerView: 1,
+            centeredSlides: true,
+            centeredSlidesBounds: true,
+            slideToClickedSlide: true,
+            effect: 'cards',
+            cardsEffect: {
+                perSlideOffset: 16
+            },
+        })
     }
 }
 </script>
 <style scoped>
+.gallery-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-top: 1rem;
+    min-height: 50vh;
+    width: 100%;
+}
+
+.swiper {
+    width: 100%;
+    max-width: calc(1000px - 1%);
+    height: 100%;
+    margin: auto;
+}
+
+.swiper-slide {
+    border-radius: 1rem;
+    width: 100%;
+    height: 100%;
+    display: flex;
+}
+
+    .swiper-slide img {
+        width: 100%;
+        object-fit: cover;
+    }
+
+    .swiper-slide p {
+        position: absolute;
+        bottom: 1rem;
+        left: 0;
+        right: 0;
+        margin: auto;
+        text-align: center;
+        color: white;
+        font-size: calc(0.4rem + 1vw);
+        font-weight: 600;
+        background: rgba(0, 0, 0, 0.7);
+        padding: calc(0.7rem + 1vw) 0;
+    }
 </style>
--- src/pages/leadershipPage.vue
+++ src/pages/leadershipPage.vue
@@ -7,7 +7,21 @@
                 <h3>Conheça as pessoas que estão à frente do ministério</h3>
             </div>
         </div>
-        <optionsBackground>            
+        <optionsBackground>       
+            <div class="p-4">
+                <div class="text">
+                    <h3 class="section-title mb-4">PASTORES TITULARES</h3>
+                </div>
+                <div class="leadership-container row">
+                    <div class="col-md-4 col-sm-12 d-flex align-items-center">
+                        <img src="../assets/img/pastores.png" class="img-fluid" />
+                    </div>
+                    <div class="col-md-8 col-sm-12 text-container d-flex justify-content-center flex-column">
+                        <h2 class="title">PRA. RAQUEL & PR. TONY</h2>
+                        <p>Casados há 21 anos e pais de três filhos, Raquel Vitória e Tony Cezar possuem uma longa caminhada juntos dentro do ministério Quadrangular. Atualmente são pastores titulares da Igreja do Evangelho Quadrangular do Farol e estão à frente dos mais diversos desafios para trazer as boas novas do evangelho.</p>
+                    </div>
+                </div>
+            </div>     
         </optionsBackground>
         <footerComponent />
     </div>
@@ -27,4 +41,10 @@ export default {
 }
 </script>
 <style scoped>
+@media (max-width: 767px) {
+    .text-container {
+        margin-top: 3rem;
+        text-align: center;
+    }
+}
 </style>
